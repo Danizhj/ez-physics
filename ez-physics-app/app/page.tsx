251dd701590091167974c0cfc91eca3a107d1fa7
Danizhj <danikhihanik@gmail.com>
2026-02-11 21:52:21 +0500

    built a working demo of application with docker conteinerization

--- ez-physics-app/app/page.tsx
+++ ez-physics-app/app/page.tsx
@@ -5,23 +5,23 @@ export default function Home() {
   return (
     <main>
       <header>
-        <Image
-          src="/icon-physics.png"
-          alt="physics icon"
-          width={70}
-          height={70}
-          className="m-4"
-        />
-        <h1 className="text-main-color font-bold text-4xl tracking-wide">
-          EZ PHYSICS
-        </h1>
+        <div className="flex justify-center items-center">
+          <Image
+            src="/icon-physics.png"
+            alt="physics icon"
+            width={70}
+            height={70}
+            className="m-4"
+          />
+          <h1 className="text-main-color font-bold text-4xl tracking-wide">
+            EZ PHYSICS
+          </h1>
+        </div>
+        <p className="text-main-color mr-7">
+          Проект сделан Жакыпбай Данияром от школы-гимназии номер 23
+        </p>
       </header>
       <Form />
-      <footer>
-        <p className="text-background-secondary text-xl font-bold">
-          Проект сделан Жакыпбай Данияром от школы-гимназии н23.
-        </p>
-      </footer>
     </main>
   );
 }
--- ez-physics-app/components/Form.tsx
+++ ez-physics-app/components/Form.tsx
@@ -14,12 +14,18 @@ const Form = () => {
     });
 
     const data = await res.json();
-    setSolution(data);
+
+    setSolution(data.solution);
   }
 
   return (
-    <div>
-      <form onSubmit={solveProblem}>
+    <div className="flex flex-col items-center">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          solveProblem();
+        }}
+      >
         <h2 className="text-main-color text-3xl font-bold mb-3">
           Введите задачу
         </h2>
@@ -32,7 +38,36 @@ const Form = () => {
           }}
         />
       </form>
-      <div>{solution}</div>
+      <div
+        className="whitespace-pre-wrap w-[80vw]"
+        dangerouslySetInnerHTML={{
+          __html: solution
+            ?.replace(
+              "Дано:",
+              '<span class="text-main-color font-bold text-xl">Дано</span><br/>',
+            )
+            .replace(
+              "СИ:",
+              '<span class="text-main-color font-bold text-xl">CИ</span><br/>',
+            )
+            .replace(
+              "Вывод формулы:",
+              '<span class="text-main-color font-bold text-xl">Вывод формулы</span><br/>',
+            )
+            .replace(
+              "Решение:",
+              '<span class="text-main-color font-bold text-xl">Решение</span><br/>',
+            )
+            .replace(
+              "Объяснение:",
+              '<span class="text-main-color font-bold text-xl">Объяснение</span><br/>',
+            )
+            .replace(
+              "Ответ:",
+              '<span class="text-main-color font-bold text-xl">Ответ</span><br/>',
+            ),
+        }}
+      ></div>
     </div>
   );
 };
